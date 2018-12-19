54f18837daf28e4b41014222b7fd84085906542b
Ivan Timofeev <ivan.timofeev@bsc-ideas.com>
2018-12-19 20:30:47 +0300

    Revert "#FEUD-12 ability to select round of game"
    This reverts commit 04f8289da7ac13492d17429847a70680ed25bd9f.

--- src/app/answers.service.ts
+++ src/app/answers.service.ts
@@ -3,17 +3,17 @@ import { HttpClient } from '@angular/common/http';
 
 @Injectable()
 export class AnswersService {
-  public round: number;
   private answers: any;
   private apiUrl: string;
 
   constructor(private http: HttpClient) {
+    this.apiUrl = '/assets/answers/answers.json';
 
+    this.getAnswers()
+      .subscribe((res) => this.answers = res);
   }
 
   public getAnswers() {
-    this.apiUrl = '/assets/answers/answers.json?round='+this.round;
-    console.log(this.apiUrl);
     return this.http.get(this.apiUrl);
   }
 }
